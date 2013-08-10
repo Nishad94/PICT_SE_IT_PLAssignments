f94349d7b3707986dbeba6f825e87439a30af22b
Nishad94 <nishad.dawkhar94@gmail.com>
2013-08-10 15:12:49 +0530

    Assignment 4 PL added

---
+++ Database/database/definitions.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "stringops.c"
+#define MAX_NAME_SIZE 30
+#define PHONENUM_SIZE 11
+#define IT 1
+#define COMP 2
+#define ENTC 3
+#define MAX_STUDENTS 10
